ed2ebef6b63e3833ddfe5b2faf9a19a48d626883
missalissa <alissa.beddow@gmail.com>
2017-12-14 11:45:32 -0800

    add new user form, create objects

--- scripts/model/items.js
+++ scripts/model/items.js
@@ -1,1 +1,19 @@
 'use strict';
+
+var app = app || {};
+
+(function(module) {
+
+    function Item (obj) {
+        this.name = obj.name;
+        this.unit = obj.unit;
+        this.type = obj.type;
+        this.amount = obj.amount
+    };
+
+    Item.all = [];
+
+    module.Item = Item;
+
+})(app);
+
